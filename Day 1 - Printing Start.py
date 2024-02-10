f5c820b80b615d7724ebd7700845df555cd24aaa
Cecelia Sciuto <ceceliasciuto@gmail.com>
2024-02-09 22:49:39 -0600

    Initial commit

---
+++ Day 1 - Printing Start.py
@@ -0,0 +1,23 @@
+# basic printing
+print("excited to start learning")
+
+# printing syntax adding new lines or strings together
+print("this is pretty cool\non a whole new level")
+print("Hey " + "You")
+
+# using inputs
+input("whats your name? ")
+
+# print an input
+print("Hey " + input("whats your name? "))
+
+# using variables
+user_name = input("whats your name? ")
+print("Hey " + user_name)
+
+# what should you name your pet project
+# greeting
+print("welcome to name your pet generator")
+color = input("What color is your pet? ")
+animal_type = input("What type of animal do you have? ")
+print("Your pets name should be " + color + " " + animal_type)
