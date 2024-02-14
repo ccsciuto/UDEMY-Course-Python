bc1ce2e083387911a4f675628f1c8dc7ca04ac9c
Cecelia Sciuto <ceceliasciuto@gmail.com>
2024-02-14 16:16:40 -0600

    Create Day 6 - Functions and Karel.py
    day six commit

---
+++ Day 6 - Functions and Karel.py
@@ -0,0 +1,21 @@
+# creating your own function
+
+def my_function():
+    print("Hello")
+    print("By")
+
+my_function()
+
+# while loops
+num = 3
+while num > 0:
+    print("Keep going")
+    num -= 1
+
+# most projects today were done on a different site so here are some notes:
+
+""" def allows you to group a list of commands as one function and can save lines of code and make it more readable
+by just calling the one function instead of needing to rewrite each step each time
+    while loops act similarly to for loops except they may not have a defined ending lkike a for loop would
+    these can be more risky because in some scenarios there is no ending ex: while 5 > 3: 5 is always greter than 3 so this
+    loop will go on forever"""
