b00233079e50f3bb5cf96c2a8aa205b7784ec51e
Cecelia Sciuto <ceceliasciuto@gmail.com>
2024-03-11 12:13:43 -0500

    Update Day 27 - TKinter.py

--- Day 27 - TKinter.py
+++ Day 27 - TKinter.py
@@ -34,6 +34,26 @@
 
 from tkinter import *
 
+
+def miles_to_km():
+    miles = float(miles_input.get())
+    km = miles * 1.609
+    km_result.config(text=f"{km}")
+
+
 window = Tk()
 window.title("First Program")
-window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
+miles_input = Entry(width=7)
+miles_input.grid(column=1, row=0)
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0)
+is_equal = Label(text="is equal to")
+is_equal.grid(column=0,row=1)
+km_result = Label(text="0")
+km_result.grid(column=1,row=1)
+km_label = Label(text="Km")
+km_label.grid(column=2,row=1)
+calc = Button(text="Convert", command=miles_to_km)
+calc.grid(column=1, row=2)
+window.mainloop()
