39cced54defedc1532343346d45a40677a1ee8ee
Cecelia Sciuto <ceceliasciuto@gmail.com>
2024-04-09 17:12:29 -0500

    Update Day 48 - Selenium.py

--- Day 48 - Selenium.py
+++ Day 48 - Selenium.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import time
 
 #
 # chrome_options = webdriver.ChromeOptions()
@@ -15,13 +16,74 @@ from selenium.webdriver.common.keys import Keys
 # print(f"The price is {price_dollar}.{price_cent}")
 # x_path = driver.find_element(By.XPATH, value='//*[@id="productTitle"]')
 # print(x_path.text)
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_experimental_option("detach", True)
+# # chrome_options.add_argument("--headless")
+# # chrome_options.add_argument("--use_subprocess")
+# driver = webdriver.Chrome(options=chrome_options)
+# driver.get("https://disneyworld.disney.go.com/attractions/#/sort=alpha/")
+#
+# name = driver.find_element(By.Link, value="Advanced Training Lab")
+# name.click()
+# driver.quit()
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 # chrome_options.add_argument("--headless")
 # chrome_options.add_argument("--use_subprocess")
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://disneyworld.disney.go.com/attractions/#/sort=alpha/")
+driver.get("https://orteil.dashnet.org/experiments/cookie/")
+cookie = driver.find_element(By.CSS_SELECTOR, value="#cookie")
+# Get upgrade item ids.
+items = driver.find_elements(by=By.CSS_SELECTOR, value="#store div")
+item_ids = [item.get_attribute("id") for item in items]
+timeout = time.time() + 5
+five_min = time.time() + 60*5
+while True:
+    cookie.click()
 
-name = driver.find_element(By.Link, value="Advanced Training Lab")
-name.click()
-driver.quit()
+    if time.time() > timeout:
+
+        # Get all upgrade <b> tags
+        all_prices = driver.find_elements(by=By.CSS_SELECTOR, value="#store b")
+        item_prices = []
+
+        # Convert <b> text into an integer price.
+        for price in all_prices:
+            element_text = price.text
+            if element_text != "":
+                cost = int(element_text.split("-")[1].strip().replace(",", ""))
+                item_prices.append(cost)
+
+        # Create dictionary of store items and prices
+        cookie_upgrades = {}
+        for n in range(len(item_prices)):
+            cookie_upgrades[item_prices[n]] = item_ids[n]
+
+        # Get current cookie count
+        money_element = driver.find_element(by=By.ID, value="money").text
+        if "," in money_element:
+            money_element = money_element.replace(",", "")
+        cookie_count = int(money_element)
+
+        # Find upgrades that we can currently afford
+        affordable_upgrades = {}
+        for cost, id in cookie_upgrades.items():
+            if cookie_count > cost:
+                affordable_upgrades[cost] = id
+
+        # Purchase the most expensive affordable upgrade
+        highest_price_affordable_upgrade = max(affordable_upgrades)
+        print(highest_price_affordable_upgrade)
+        to_purchase_id = affordable_upgrades[highest_price_affordable_upgrade]
+
+        driver.find_element(by=By.ID, value=to_purchase_id).click()
+
+        # Add another 5 seconds until the next check
+        timeout = time.time() + 5
+
+        # After 5 minutes stop the bot and check the cookies per second count.
+    if time.time() > five_min:
+        cookie_per_s = driver.find_element(by=By.ID, value="cps").text
+        print(cookie_per_s)
+        break
